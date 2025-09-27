26b5074edb508abdde601e6c827e94dc5c752435
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-27 15:49:47 +0330

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Chào mừng đến với Phân Tích Dữ Liệu Sinh Thái!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server đang chạy trên http://localhost:${PORT}`);
+});
