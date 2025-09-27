9cb17b1a86c362d0550ce3e897ce07c736f7b963
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-27 15:49:48 +0330

    feat: initial commit for model/Data.js

---
+++ model/Data.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+  sensorId: { type: String, required: true },
+  value: { type: Number, required: true },
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Data', dataSchema);
