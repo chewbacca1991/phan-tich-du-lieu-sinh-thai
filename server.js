23039d3eb3a7a4e41a0c31320dfeb142f5873289
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-27 15:53:04 +0330

    Update welcome message to reflect new version

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Ecological Data Analysis! Version 1.0');
+  res.send('Welcome to Ecological Data Analysis! Version 1.1');
 });
 
 app.listen(PORT, () => {
